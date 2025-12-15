b80a0aead1c84d0cf7bea2c2a77e5fbc5a32395b
junghee111111 <jungheejjang1004@gmail.com>
2025-12-16 07:49:17 +0900

    fix: build error 2

--- Source/DigitalBleed/System/DigitalBleedGameInstance.cpp
+++ Source/DigitalBleed/System/DigitalBleedGameInstance.cpp
@@ -4,10 +4,7 @@
 #include "DigitalBleedGameInstance.h"
 
 #include "Blueprint/UserWidget.h"
-<<<<<<< HEAD
 #include "Kismet/GameplayStatics.h"
-=======
->>>>>>> bebc240abf8aec06802bfe897c1d6c7e0b995438
 
 void UDigitalBleedGameInstance::InitGamePlayerLoggedIn()
 {
