5427ec10df8524056c6a5eb92a7c40a218a92e09
junghee111111 <jungheejjang1004@gmail.com>
2025-12-25 16:45:58 +0900

    wip: dialog selection button display

--- Source/DigitalBleed/UI/WidgetDialogSelection.cpp
+++ Source/DigitalBleed/UI/WidgetDialogSelection.cpp
@@ -4,6 +4,7 @@
 #include "WidgetDialogSelection.h"
 
 #include "Components/TextBlock.h"
+#include "Components/VerticalBox.h"
 
 void UWidgetDialogSelection::NativeConstruct()
 {
@@ -14,6 +15,16 @@ void UWidgetDialogSelection::SetSelectionData(const FRowSelection& NewSelectionD
 {
 	this->SelectionData = NewSelectionData;
 	this->Text_Title->SetText(this->SelectionData.Title);
+	this->VBox_Selections->ClearChildren();
+
+	APlayerController* PC = GetGameInstance()->GetFirstLocalPlayerController();
+	
+	for (int32 i = 0; i < this->SelectionData.Actions.Num(); i++)
+	{
+		UWidgetDialogSelectionButton* WbpButton = CreateWidget<class UWidgetDialogSelectionButton>(PC,WbpDialogSelectionButtonClass);
+		WbpButton->SetSelectionData(this->SelectionData.Body[i], this->SelectionData.Actions[i]);
+		this->VBox_Selections->AddChildToVerticalBox(WbpButton);
+	}
 }
 
 FRowSelection& UWidgetDialogSelection::GetSelectionData()
